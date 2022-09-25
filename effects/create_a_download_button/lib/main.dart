1cffd68bd4f9a4b01b8d889c5306ded18a13440a
skyturkish <gokturk.acr2002@gmail.com>
2022-09-26 01:12:06 +0300

    form example

--- effects/create_a_download_button/lib/main.dart
+++ effects/create_a_download_button/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// TODO will investigaion detaily
 void main() {
   runApp(
     const MaterialApp(
---
+++ forms/build_a_form_with_validation/android/app/src/main/kotlin/sky/sky/build_a_form_with_validation/MainActivity.kt
@@ -0,0 +1,6 @@
+package sky.sky.build_a_form_with_validation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
